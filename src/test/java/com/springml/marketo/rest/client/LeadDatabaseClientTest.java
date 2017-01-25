0ea350f62c189079d3cb546ccce2dc2eaa01e25c
Samuel Alexander <samuel@palmtreeinfotech.com>
2017-01-25 18:40:57 +0530

    Simplified the test

--- src/test/java/com/springml/marketo/rest/client/LeadDatabaseClientTest.java
+++ src/test/java/com/springml/marketo/rest/client/LeadDatabaseClientTest.java
@@ -9,6 +9,8 @@ import java.util.*;
 
 import static com.springml.marketo.rest.client.util.MarketoClientConstants.*;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 /**
@@ -18,7 +20,7 @@ import static org.mockito.Mockito.when;
 public class LeadDatabaseClientTest extends BaseDatabaseClientTest {
 
     private String response;
-    private String EmptyResponse;
+    private String emptyResponse;
 
     @Before
     public void setUp() throws Exception {
@@ -28,7 +30,7 @@ public class LeadDatabaseClientTest extends BaseDatabaseClientTest {
 
         ClassLoader classLoader = getClass().getClassLoader();
         response = IOUtils.toString(classLoader.getResourceAsStream("leadResponse.json"), "UTF-8");
-        EmptyResponse = IOUtils.toString(classLoader.getResourceAsStream("leadEmptyResponse.json"),
+        emptyResponse = IOUtils.toString(classLoader.getResourceAsStream("leadEmptyResponse.json"),
                 "UTF-8");
 
         when(httpHelper.get(MARKETO_BASEURI, "/rest/v1/leads.json", "e4ceb18c-f578-4ecb:sj", params)).
@@ -55,7 +57,7 @@ public class LeadDatabaseClientTest extends BaseDatabaseClientTest {
     public void testQueryLeadsWithFields() throws Exception {
 
         QueryResult actualQueryResult = leadDatabaseClient.query("leads", "Id", "10",
-                Arrays.asList("email"));
+                Collections.singletonList("email"));
 
         List<Map<String, String>> expectedResults = getExpectedResultMaps();
         assertEquals(expectedResults, actualQueryResult.getResult());
@@ -63,50 +65,18 @@ public class LeadDatabaseClientTest extends BaseDatabaseClientTest {
 
     @Test
     public void testQueryLeadsForAllRecords() throws Exception {
-
-        Map<String, String> paramsUpto300 = new HashMap<>();
-        paramsUpto300.put(STR_FILTER_TYPE, "Id");
-        StringBuilder filterValueUpto300 = getStringBuilderUpto300(1);
-        paramsUpto300.put(STR_FILTER_VALUES, String.valueOf(filterValueUpto300));
-
-        Map<String, String> paramsUpto600 = new HashMap<>();
-        paramsUpto600.put(STR_FILTER_TYPE, "Id");
-        StringBuilder filterValueUpto600 = getStringBuilderUpto600(301);
-        paramsUpto600.put(STR_FILTER_VALUES, String.valueOf(filterValueUpto600));
-
-        when(httpHelper.get(MARKETO_BASEURI, "/rest/v1/leads.json", "e4ceb18c-f578-4ecb:sj", paramsUpto300)).
-                thenReturn(response);
-
-        when(httpHelper.get(MARKETO_BASEURI, "/rest/v1/leads.json", "e4ceb18c-f578-4ecb:sj", paramsUpto600)).
-                thenReturn(EmptyResponse);
+        when(httpHelper.get(eq(MARKETO_BASEURI), eq("/rest/v1/leads.json"), eq("e4ceb18c-f578-4ecb:sj"), any(Map.class))).
+                thenReturn(response).thenReturn(emptyResponse);
 
         List<Map<String, String>> actualQueryResults = leadDatabaseClient.getAllRecords("leads");
-
         List<Map<String, String>> expectedResults = getExpectedResultMaps();
-
         assertEquals(expectedResults, actualQueryResults);
     }
 
     @Test
     public void testQueryLeadsForAllRecordsWithFields() throws Exception {
-
-        Map<String, String> paramsUpto300 = new HashMap<>();
-        paramsUpto300.put(STR_FILTER_TYPE, "Id");
-        StringBuilder filterValueUpto300 = getStringBuilderUpto300(1);
-        paramsUpto300.put(STR_FILTER_VALUES, String.valueOf(filterValueUpto300));
-        paramsUpto300.put(STR_FIELDS, "email");
-
-        Map<String, String> paramsUpto600 = new HashMap<>();
-        paramsUpto600.put(STR_FILTER_TYPE, "Id");
-        StringBuilder filterValueUpto600 = getStringBuilderUpto600(301);
-        paramsUpto600.put(STR_FILTER_VALUES, String.valueOf(filterValueUpto600));
-        paramsUpto600.put(STR_FIELDS, "email");
-
-        when(httpHelper.get(MARKETO_BASEURI, "/rest/v1/leads.json", "e4ceb18c-f578-4ecb:sj", paramsUpto300)).
-                thenReturn(response);
-
-        when(httpHelper.get(MARKETO_BASEURI, "/rest/v1/leads.json", "e4ceb18c-f578-4ecb:sj", paramsUpto600)).
-                thenReturn(EmptyResponse);
+        when(httpHelper.get(eq(MARKETO_BASEURI), eq("/rest/v1/leads.json"), eq("e4ceb18c-f578-4ecb:sj"), any(Map.class))).
+                thenReturn(response).thenReturn(emptyResponse);
 
         List<Map<String, String>> actualQueryResults = leadDatabaseClient.getAllRecords("leads", Arrays.asList("email"));
         List<Map<String, String>> expectedResults = getExpectedResultMaps();
@@ -171,32 +141,6 @@ public class LeadDatabaseClientTest extends BaseDatabaseClientTest {
         results.add(result1);
         results.add(result2);
         return results;
-    }
-
-    private StringBuilder getStringBuilderUpto300(int count) {
-        StringBuilder filterValueUpto300 = new StringBuilder();
-        for (int i = 0; i < 300; i++) {
-            filterValueUpto300.append(count);
-            if (i != 299) {
-                filterValueUpto300.append(",");
-            }
-
-            count++;
-        }
-        return filterValueUpto300;
-    }
-
-    private StringBuilder getStringBuilderUpto600(int count) {
-        StringBuilder filterValueUpto600 = new StringBuilder();
-        for (int i = 300; i < 600; i++) {
-            filterValueUpto600.append(count);
-            if (i != 599) {
-                filterValueUpto600.append(",");
-            }
-
-            count++;
-        }
-        return filterValueUpto600;
     }
 
     private List<Map<String, String>> getExpectedResultMaps() {
